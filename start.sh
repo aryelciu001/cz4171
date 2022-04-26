977746934ae391383ef861fbfa69cfaff508209a
aryelciu001 <ryelcius.alfredo29@gmail.com>
2022-04-26 20:14:55 +0800

    try to work

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 pkill -f node
 yes | cp -rf ./nginx.conf /etc/nginx
 rm -r /var/www/html/*
-cp -a ./client/build /var/www/html
+cp -a ./client/build/. /var/www/html/
 sudo systemctl restart nginx
 cd server 
 npm start
