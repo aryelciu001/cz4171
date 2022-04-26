c28122bd2bfe5a9f72f1f2de43c87b21630b9f74
aryelciu001 <ryelcius.alfredo29@gmail.com>
2022-04-26 17:24:54 +0800

    add styling

--- client/src/App.js
+++ client/src/App.js
@@ -6,22 +6,53 @@ const URL = "http://localhost:3001";
 
 function App() {
   const upload = () => {
+    if (!ref) return;
     const formData = new FormData();
     const config = {
       headers: { "content-type": "multipart/form-data" },
     };
     formData.append("picture", ref.current.files[0]);
-    axios.post(URL, formData, config).then((res) => setText(res.data));
+    setLoading(true);
+    axios.post(URL, formData, config).then((res) => {
+      setText(res.data);
+      setLoading(false);
+    });
   };
 
   const ref = useRef(null);
   const [text, setText] = useState("");
+  const [name, setName] = useState("");
+  const [loading, setLoading] = useState(false);
 
   return (
     <div className="App">
-      <input ref={ref} type="file" accept="image/*"></input>
-      <button onClick={() => upload()}>Upload</button>
-      <div className="text">{text}</div>
+      <h1 className="row">CZ4171</h1>
+      <div className="row">Upload an image to read what the image says</div>
+      <input
+        onChange={() => setName(ref.current.files[0].name)}
+        id="picture"
+        ref={ref}
+        type="file"
+        accept="image/*"
+        hidden
+      ></input>
+      {name ? <div className="row">{name}</div> : null}
+      <div className="buttons">
+        <label htmlFor="picture" className="row">
+          Upload File
+        </label>
+        <button onClick={() => upload()} className="row">
+          Read File
+        </button>
+      </div>
+      {loading ? <div className="row">reading text...</div> : null}
+      {text ? (
+        <div className="row text">
+          <span>Result</span>
+          <hr></hr>
+          {text}
+        </div>
+      ) : null}
     </div>
   );
 }
