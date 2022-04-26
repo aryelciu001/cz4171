a71faf41b9bcaed523f76264fcc4d01463fa9506
aryelciu001 <ryelcius.alfredo29@gmail.com>
2022-04-26 21:31:37 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# Requirement
+
+1. Linux
+2. NodeJS
+3. npm
+4. NGINX
+
+# How to run
+
+`sudo sh start.sh`
--- start.sh
+++ start.sh
@@ -1,10 +1,12 @@
 pkill -f node
 yes | cp -rf ./nginx.conf /etc/nginx
 rm -r /var/www/html/*
-cd client 
+cd client
+npm install
 npm run build
 (npm run deploy&)
 sudo systemctl restart nginx
 cd ..
 cd server 
+npm install
 (npm run start&)
