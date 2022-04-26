50d81babc88cebb0652e64d9b612cce840cf823a
aryelciu001 <ryelcius.alfredo29@gmail.com>
2022-04-26 21:33:02 +0800

    add readme

--- start.sh
+++ start.sh
@@ -1,11 +1,19 @@
+# kill running node, if any
 pkill -f node
+
+# copy nginx config
 yes | cp -rf ./nginx.conf /etc/nginx
-rm -r /var/www/html/*
+
+# prepare client
 cd client
 npm install
 npm run build
 (npm run deploy&)
+
+# restart nginx, if running
 sudo systemctl restart nginx
+
+# prepare server
 cd ..
 cd server 
 npm install
