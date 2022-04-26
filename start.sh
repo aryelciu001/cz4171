513eb15800c720aaf8ba092d78a9de2bd093ab9e
aryelciu001 <ryelcius.alfredo29@gmail.com>
2022-04-26 19:47:33 +0800

    modify nginx.conf

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
+pkill -f node
 yes | cp -rf ./nginx.conf /etc/nginx
 mkdir /var/www
-cp ./client/build /var/www
+yes | cp ./client/build /var/www
 sudo systemctl restart nginx
 cd server 
 npm start
